02fe42dc9db61e4a669b87428b4873ef67f19f3b
yusufva <yusufvalent@gmail.com>
2024-02-27 11:03:01 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Day 2 Assignment
+// Creating Hello World Java App
+// Name : Yusuf Valent Adyatomo
+// Gen - 20
+
+
+// Class utama
+public class Main {
+
+//  Method Main tempat berisinya program utama yang akan dijalankan
+    public static void main(String[] args) {
+
+        // System.out.printf berfungsi untuk menampilkan / melakukan print suatu string ataupun variable
+        System.out.println("Hello world!!");
+
+    }
+}
